3586408c367eae734dcd36a7b845da9921d0fc2f
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-05 19:09:00 +0600

    first commit

---
+++ app/about/info/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutInfoPage = () => {
+  return (
+    <div>AboutInfo Page</div>
+  )
+}
+
+export default AboutInfoPage
---
+++ components/NavBar.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+const links = [
+  {
+    href: "/client",
+    label: "client",
+  },
+  {
+    href: "/drinks",
+    label: "drinks",
+  },
+  {
+    href: "/query",
+    label: "query",
+  },
+  {
+    href: "/tasks",
+    label: "tasks",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="bg-base-300 py-4">
+      <div className="navbar px-8 max-w-6xl max-auto flex-col">
+        <Link href="/" className="btn btn-primary">
+          Next.js
+        </Link>
+
+        <ul className="menu menu-horizontal">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href} className="capitalize">{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
