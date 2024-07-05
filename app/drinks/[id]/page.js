d42fec2605ff6243bdfcb6f13e1f4e124c4ae400
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-05 21:51:46 +0600

    single id shown

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
@@ -13,14 +14,21 @@ const getSingleDrink = async (id) => {
 const SingleDrink = async ({ params }) => {
   const data = await getSingleDrink(params.id);
   const title = data?.drinks[0]?.strDrink;
-  const imgSrc = data?.drinks[0]?.strDrinkThumb;
+  const drinkImg = data?.drinks[0]?.strDrinkThumb;
   return (
     <div>
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         back to drinks
       </Link>
       <h1 className="text-4xl mb-8">{title}</h1>
-      <img src={imgSrc} />
+      <Image
+        src={drinkImg}
+        width={300}
+        height={300}
+        className="w-48 h-48 rounded shadow-lg mb-4"
+        priority
+        alt=""
+      />
     </div>
   );
 };
