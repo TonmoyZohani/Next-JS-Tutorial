dc1d3c9f8bef771bc82959e1fcaa32bcb2fa78e5
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-05 21:55:10 +0600

    single id shown

---
+++ next.config.js
@@ -0,0 +1,15 @@
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.thecocktaildb.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
