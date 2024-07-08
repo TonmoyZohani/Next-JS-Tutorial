25eba2b2361485deba952b40c12680baf3355c32
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-08 22:15:16 +0600

    link comment added

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
 const HomePage = () => {
-
-// This is the route page
+  // This is the route page
 
   return (
     <div>
       <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+
+      This is link
       <Link href="/client" className="btn btn-accent">
-       Get Started
+        Get Started
       </Link>
     </div>
   );
