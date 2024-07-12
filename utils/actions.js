b73d2eb22fb2830a0f1c0afc462f41b2828363d0
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-12 21:57:36 +0600

    create actions

--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,6 @@
 "use server";
-
 import prisma from "./db";
+import { revalidatePath } from "next/cache";
 
 export const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -8,4 +8,16 @@ export const getAllTasks = async () => {
       createdAt: "desc",
     },
   });
+};
+
+export const createTask = async (formData) => {
+  const content = formData.get("content");
+
+  await prisma.task.create({
+    data: {
+      content: content,
+    },
+  });
+
+  revalidatePath("/tasks");
 };
