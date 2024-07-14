cdef9ac1de499a9615438ea97e9aceee155d4a4d
Khaled Zohani Tonmoy <94390552+TonmoyZohani@users.noreply.github.com>
2024-07-14 09:11:16 +0600

    Taskform created

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,22 +1,38 @@
-import { createTask } from "@/utils/actions";
+'use client';
+
+import { createTaskCustom } from '@/utils/actions';
+import { useFormStatus } from 'react-dom';
+// The useFormStatus Hook provides status information of the last form submission.
+// useFormState is a Hook that allows you to update state based on the result of a form action.
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type='submit'
+      className='btn join-item btn-primary'
+      disabled={pending}
+    >
+      {pending ? 'please wait... ' : 'create task'}
+    </button>
+  );
+};
 
 const TaskForm = () => {
   return (
-    <form action={createTask}>
-      <div className="join w-full">
+    <form action={createTaskCustom}>
+      <div className='join w-full'>
         <input
-          className="input input-bordered join-item w-full"
-          placeholder="Type Here"
-          type="text"
-          name="content"
+          className='input input-bordered join-item w-full'
+          placeholder='Type Here'
+          type='text'
+          name='content'
           required
         />
-        <button type="submit" className="btn join-item btn-primary">
-          create task
-        </button>
+        <SubmitButton />
       </div>
     </form>
   );
 };
-
 export default TaskForm;
