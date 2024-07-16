f3eb6be590f2b2b5ece09b4bbcde91bb7f5ad8cf
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-16 19:33:15 +0600

    providers component created

---
+++ providers.js
@@ -0,0 +1,14 @@
+"use-client";
+
+import { Toaster } from "react-hot-toast";
+
+const providers = ({ children }) => {
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
+};
+
+export default providers;
