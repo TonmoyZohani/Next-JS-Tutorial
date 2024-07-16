4183a47d562f7720afbc403e961e63424770a7c7
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-16 19:45:26 +0600

    toase added

--- utils/actions.js
+++ utils/actions.js
@@ -46,10 +46,10 @@ export const createTaskCustom = async (prevState, formData) => {
     });
     // revalidate path
     revalidatePath("/tasks");
-    return { message: "success!!!" };
+    return { message: "success" };
   } catch (error) {
     // can't return error
-    return { message: "error..." };
+    return { message: "error" };
   }
 };
 
